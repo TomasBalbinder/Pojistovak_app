5b145dfc3ca5c7c9e87a36398839e455d9bdb784
Tomas <tomasbalbinder@gmail.com>
2024-06-01 20:02:20 +0200

    'second'

--- build.sh
+++ build.sh
@@ -14,18 +14,5 @@ python manage.py migrate
 if [[ $CREATE_SUPERUSER ]]; then
     echo "Vytvářím superuživatele..."
     python manage.py createsuperuser --no-input --username "$DJANGO_SUPERUSER_USERNAME" --email "$DJANGO_SUPERUSER_EMAIL"
-    
-    echo "Kontroluji a vytvářím UserProfile..."
-    python << END
-import os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pojistovna.settings')
-import django
-django.setup()
-from django.contrib.auth.models import User
-from evidence_pojisteni_app.models import UserProfile
-
-username = os.getenv('DJANGO_SUPERUSER_USERNAME')
-user = User.objects.get(username=username)
-UserProfile.objects.get_or_create(user=user)
-END
 fi
+
--- evidence_pojisteni_app/models.py
+++ evidence_pojisteni_app/models.py
@@ -47,12 +47,13 @@ class UserProfile(models.Model):
     
     @receiver(post_save, sender=User)
     def create_user_profile(sender, instance, created, **kwargs):
-        if created:
-            UserProfile.objects.create(user=instance)
+        if created and not instance.is_superuser:
+            UserProfile.objects.get_or_create(user=instance)
 
     @receiver(post_save, sender=User)
     def save_user_profile(sender, instance, **kwargs):
-        instance.userprofile.save()
+        if not instance.is_superuser:
+            instance.userprofile.save()
       
 
 class Customer(models.Model):
