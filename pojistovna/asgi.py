9ebd07fbb77ff4ebc331d2dc7b0b948570e270a5
Tomas <tomasbalbinder@gmail.com>
2024-06-01 11:23:56 +0200

    'second'

--- pojistovna/asgi.py
+++ pojistovna/asgi.py
@@ -10,10 +10,7 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-from uvicorn.middleware.proxy_headers import ProxyHeadersMiddleware
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pojistovna.settings')
 
 application = get_asgi_application()
-application = ProxyHeadersMiddleware(django_asgi_application, trusted_hosts=["evidence_pojisteni.app.onrender.com"])
-
-
