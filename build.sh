2835a614674d260e13f8378cc8742103b6757e44
Tomas <tomasbalbinder@gmail.com>
2024-06-01 15:00:32 +0200

    'second'

--- build.sh
+++ build.sh
@@ -10,5 +10,3 @@ python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python manage.py migrate
-
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('Tomas', 'tomasbalbinder@gmail.com', 'Idefixa')" | python manage.py shell
