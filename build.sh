c911e8cb90032ce7b5f6e696f3110da7cda9cc54
Tomas <tomasbalbinder@gmail.com>
2024-06-01 14:45:31 +0200

    'second'

--- build.sh
+++ build.sh
@@ -11,18 +11,4 @@ python manage.py collectstatic --no-input
 # Apply any outstanding database migrations
 python manage.py migrate
 
-python manage.py shell <<EOF
-from django.contrib.auth.models import User
-from evidence_pojisteni_app.models import UserProfile  # Importujte váš model UserProfile
-
-username = 'admin'
-email = 'tomasbalbinder@gmail.com'
-password = 'Idefixa'
-
-if not User.objects.filter(username=username).exists():
-    user = User.objects.create_superuser(username, email, password)
-    UserProfile.objects.create(user=user)  # Vytvoříme uživatelský profil
-    print(f'Superuser {username} created with profile')
-else:
-    print(f'Superuser {username} already exists')
-EOF
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'tomasbalbinder@gmail.com', 'Idefixa')" | python manage.py shell
