b67e8d0199f0ff670403a3ed20e715b59b66fe97
Tomas <tomasbalbinder@gmail.com>
2024-06-01 16:54:54 +0200

    'second'

--- pojistovna/settings.py
+++ pojistovna/settings.py
@@ -17,6 +17,10 @@ ADMIN_LOGIN = 'pepicek'
 ADMIN_PASSWORD = 'pepicek'
 
 
+AUTHENTICATION_BACKENDS = [
+    'evidence_pojisteni_app.backends.SettingsBackend',
+    'django.contrib.auth.backends.ModelBackend',
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
