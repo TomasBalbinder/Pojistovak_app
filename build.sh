f989fd92a2b65806e54bf51c2f46261736e7d4f3
Tomas <tomasbalbinder@gmail.com>
2024-06-01 19:12:14 +0200

    'second'

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ python manage.py migrate
 
 if [[ $CREATE_SUPERUSER ]];
 then
-    python manage.py createsuperuser --no-input
-    -username $DJANGO_SUPERUSER_USERNAME \
-    --email $DJANGO_SUPERUSER_EMAIL 
+    python manage.py createsuperuser --no-input \
+    -username "$DJANGO_SUPERUSER_USERNAME" \
+    --email "$DJANGO_SUPERUSER_EMAIL" 
 fi
