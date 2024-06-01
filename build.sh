aa328c126932ca39a09105983c29fc24b38cb4a7
Tomas <tomasbalbinder@gmail.com>
2024-06-01 18:57:19 +0200

    'second'

--- build.sh
+++ build.sh
@@ -14,4 +14,6 @@ python manage.py migrate
 if [[ $CREATE_SUPERUSER ]];
 then
     python manage.py createsuperuser --no-input
+    -username $DJANGO_SUPERUSER_USERNAME \
+    --email $DJANGO_SUPERUSER_EMAIL 
 fi
--- evidence_pojisteni_app/models.py
+++ evidence_pojisteni_app/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 import random
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class KindOfInsurance(models.Model):
@@ -42,6 +44,15 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return f"{self.user.username}"
+    
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            UserProfile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.userprofile.save()
       
 
 class Customer(models.Model):
